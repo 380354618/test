57e97667e0284ec147d8f2e93dd56191389adf30
maizhijian <380354618@qq.com>
2021-01-10 17:19:15 +0800

    222

---
+++ src/Maizhijian/Mais.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Maizhijian;
+
+class Mais
+{
+    public function test()
+    {
+        echo 123;
+    }
+
+
+    public function test2()
+    {
+        echo 1111111111111111111111111;
+    }
+}
